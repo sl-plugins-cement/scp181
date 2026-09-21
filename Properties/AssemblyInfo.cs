62f73f21dd954f527e50aab101c693ff04515a1e
114514Pro-dev <2474556050@qq.com>
2026-09-21 18:19:59 +0800

    Add Properties/AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Scp181")]
+[assembly: AssemblyDescription("SCP-181 角色插件")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ArcaneStrike")]
+[assembly: AssemblyProduct("Scp181")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8E1A0B3C-7D21-4F99-9B0C-5D2E9A1F4C8D")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
